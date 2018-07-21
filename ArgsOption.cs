d5b13b4a77fd732a43649002b4e4ed72472ce65d
Hongjiang Zhang <honzhan@microsoft.com>
2018-07-21 18:21:14 +0800

    initialize the project

---
+++ ArgsOption.cs
@@ -0,0 +1,19 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VerifyCookie
+{
+    class ArgsOption
+    {
+        [Option("hub", Required = true, HelpText = "Specify hub url, i.e. http://signalrlinux3.southeastasia.cloudapp.azure.com:5050/signalrbench")]
+        public string HubUrl { get; set; }
+
+        [Option("transport", Required = false, Default = "Websockets", HelpText = "Specify the transport type: Websockets|LongPolling|ServerSentEvents")]
+        public string TransportType { get; set; }
+
+        [Option("useHandler", Required = false, Default = "true", HelpText = "Enable HttpHanlder which triggers cookie missing issue")]
+        public string UseHandler { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,92 @@
+﻿using CommandLine;
+using Microsoft.AspNetCore.Http.Connections;
+using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace VerifyCookie
+{
+    class Program
+    {
+        static async Task VerifyCookie(string url, HttpTransportType transportType, bool addHandler)
+        {
+            var httpClientHandler = new HttpClientHandler
+            {
+                ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator,
+                MaxConnectionsPerServer = 200000,
+                MaxAutomaticRedirections = 200000
+            };
+            var hubConnectionBuilder = new HubConnectionBuilder()
+                .ConfigureLogging(logging =>
+                {
+                    logging.AddConsole();
+                })
+                .WithUrl(url, httpConnectionOptions =>
+                {
+                    if (addHandler)
+                    {
+                        httpConnectionOptions.HttpMessageHandlerFactory = _ => httpClientHandler;
+                    }
+                    httpConnectionOptions.Transports = transportType;
+                    httpConnectionOptions.CloseTimeout = TimeSpan.FromMinutes(100);
+                });
+            var connection = hubConnectionBuilder.Build();
+            var method = "echo";
+            var received = false;
+            connection.On(method, (string uid, string time) =>
+            {
+                Console.WriteLine($"receive {uid} {time}");
+                received = true;
+            });
+            CancellationTokenSource closedTokenSource = null;
+
+            connection.Closed += e =>
+            {
+                // This should never be null by the time this fires
+                closedTokenSource.Cancel();
+
+                Console.WriteLine("Connection closed...");
+                return Task.CompletedTask;
+            };
+            await Task.WhenAll(connection.StartAsync());
+            await connection.SendAsync(method, "abc", DateTimeOffset.UtcNow.ToUnixTimeMilliseconds());
+            closedTokenSource = new CancellationTokenSource();
+            while (!received && !closedTokenSource.Token.IsCancellationRequested)
+            {
+                await Task.Delay(1000);
+                Console.WriteLine("Waiting for result");
+            }
+            closedTokenSource?.Dispose();
+        }
+        static void Main(string[] args)
+        {
+            bool isInputValid = true;
+            var argsOption = new ArgsOption();
+            var result = Parser.Default.ParseArguments<ArgsOption>(args)
+                .WithParsed(options => argsOption = options)
+                .WithNotParsed(error => {
+                    isInputValid = false;
+                    Console.WriteLine($"Input is invalid for {error}");
+                });
+            if (!isInputValid)
+            {
+                return;
+            }
+
+            var transportType = HttpTransportType.WebSockets;
+            switch (argsOption.TransportType)
+            {
+                case "LongPolling":
+                    transportType = HttpTransportType.LongPolling;
+                    break;
+                case "ServerSentEvents":
+                    transportType = HttpTransportType.ServerSentEvents;
+                    break;
+            }
+            VerifyCookie(argsOption.HubUrl, transportType, Boolean.Parse(argsOption.UseHandler)).GetAwaiter().GetResult();
+        }
+    }
+}
